f3cffecbe6478f043fc74abb637892c46ead72f5
MediconcenVictor <victor_chang@mediconcen.com>
2023-09-20 23:39:21 +0800

    remove build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Input from "./components/Input";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className=" text-pink-700 space-y-3">
