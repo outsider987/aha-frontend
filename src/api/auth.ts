3ed5bd2461137e8b8fb5e9d9f9ebd1f545fe0dc2
MediconcenVictor <victor_chang@mediconcen.com>
2023-09-27 00:52:21 +0800

    clear cookies

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -24,7 +24,7 @@ const useAuthApi = () => {
     const resp = await authApi.post<APIResponse>('login', {
       ...userDate,
     },
-    {withCredentials: true, headers: { 'Content-Type': 'application/json','SameSite': 'none' }
+    {withCredentials: true, headers: { 'Content-Type': 'application/json' }
     
   });
     if (resp.data.data) setTokenStorage(resp.data.data);
